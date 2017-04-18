7ca07e007f33fbd05cf1c0d568bf83126374e53e
CJayson <402288209@qq.com>
2017-04-18 23:04:16 +0800

    modfity

--- header.php
+++
@@ -1,8 +0,0 @@
-
-<head>
-    <meta http-equiv="Content-Type" content="text/html"; charset="UTF-8">
-    <title></title>
-    <link href="css/css.css" rel="stylesheet"type="text/css">
-    <b><a href="adddb.php">添加留言</a>|<a href="list.php">浏览留言</a>|<a href="login.php">登陆</a></b>
-    <hr size=1>
-</head>
