5b4bd7f20efe3ce1947a0c443356bd1b98c0a7af
CJayson <402288209@qq.com>
2017-04-18 21:14:26 +0800

    first backup

---
+++ mysql_connect.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 2017/4/7
+ * Time: 17:28
+ */
+$host="localhost";
+$db_user="root";
+$db_pass="root";
+$db_name="test";
+$timezone="Asia/Shanghai";
+
+$con=@mysqli_connect($host,$db_user,$db_pass) or die("链接失败");
+mysqli_select_db($con, $db_name);
+
+
+header("Content-Type:text/html;charset=utf-8");
+date_default_timezone_set($timezone);
+?>
